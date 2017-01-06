0fac208e357302e4e941eb1e3e82ad4bb92f4d5f
Jing Li <lijing@example.com>
2017-01-06 14:44:17 +0800

    Add C# program

---
+++ AutoProxy/AutoProxy.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace AutoProxy
+{
+    partial class AutoProxy
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btpac = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.PacAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btserver = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btpac
+            // 
+            this.btpac.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btpac.Location = new System.Drawing.Point(390, 84);
+            this.btpac.Name = "btpac";
+            this.btpac.Size = new System.Drawing.Size(88, 23);
+            this.btpac.TabIndex = 0;
+            this.btpac.Text = "设置自动代理";
+            this.btpac.UseVisualStyleBackColor = true;
+            this.btpac.Click += new System.EventHandler(this.startup_Click);
+            this.btpac.MouseLeave += new System.EventHandler(this.PAC_tip);
+            // 
+            // stop
+            // 
+            this.stop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.stop.Location = new System.Drawing.Point(486, 84);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(86, 23);
+            this.stop.TabIndex = 0;
+            this.stop.Text = "停止系统代理";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // PacAddress
+            // 
+            this.PacAddress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PacAddress.Location = new System.Drawing.Point(14, 43);
+            this.PacAddress.Name = "PacAddress";
+            this.PacAddress.Size = new System.Drawing.Size(558, 21);
+            this.PacAddress.TabIndex = 1;
+            this.PacAddress.TextChanged += new System.EventHandler(this.PacAddress_TextChanged);
+            this.PacAddress.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PacAddress_TextEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "代理服务器地址或PAC URL:";
+            // 
+            // btserver
+            // 
+            this.btserver.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btserver.Location = new System.Drawing.Point(279, 84);
+            this.btserver.Name = "btserver";
+            this.btserver.Size = new System.Drawing.Size(101, 23);
+            this.btserver.TabIndex = 0;
+            this.btserver.Text = "设置代理服务器";
+            this.btserver.UseVisualStyleBackColor = true;
+            this.btserver.Click += new System.EventHandler(this.btserver_Click);
+            this.btserver.MouseHover += new System.EventHandler(this.ProxyServer_Tip);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 139);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(584, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // AutoProxy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 161);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PacAddress);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.btserver);
+            this.Controls.Add(this.btpac);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(600, 200);
+            this.MinimumSize = new System.Drawing.Size(600, 200);
+            this.Name = "AutoProxy";
+            this.Text = "AutoProxy";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btpac;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.TextBox PacAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btserver;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+    }
+}
+
---
+++ AutoProxy/AutoProxy.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+
+namespace AutoProxy
+{
+    public partial class AutoProxy : Form
+    {
+        // HKEY_CURRENT_USER
+        private const string InternetOpt = "Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings";
+        RegistryKey InternetKey;
+
+        private string ProxyPacURL, ProxyServer;
+        private int ProxyEnable;
+        
+        [DllImport("wininet.dll")]
+        public static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int dwBufferLength);
+        public const int INTERNET_OPTION_SETTINGS_CHANGED = 39;
+        public const int INTERNET_OPTION_REFRESH = 37;
+
+        public AutoProxy()
+        {   
+            InitializeComponent();
+
+            InternetKey = Registry.CurrentUser.OpenSubKey(InternetOpt, true);
+
+            ProxyPacURL = (string)InternetKey.GetValue("AutoConfigURL", "");
+            ProxyServer = (string)InternetKey.GetValue("ProxyServer", "");
+            ProxyEnable = (int)InternetKey.GetValue("ProxyEnable");
+
+            StatusLabel.Text = "No setting";
+            if (ProxyPacURL.Length > 0)
+            {
+                btpac.Enabled = false;
+                PacAddress.Text = ProxyPacURL;
+                StatusLabel.Text = "Auto Proxy set";
+            }
+            else if (ProxyServer.Length > 0)
+            {
+                PacAddress.Text = ProxyServer;
+            }
+            if (ProxyEnable != 0)
+            {
+                btserver.Enabled = false;
+                PacAddress.Text = ProxyServer;
+                StatusLabel.Text = "Proxy Server set";
+            }
+        }
+
+        private void btserver_Click(object sender, EventArgs e)
+        {
+            if (PacAddress.Text.Length == 0)
+                return;
+
+            DialogResult result = MessageBox.Show("Address is " + PacAddress.Text, "代理服务器", MessageBoxButtons.YesNo);
+            if (result != System.Windows.Forms.DialogResult.Yes)
+                return;
+
+            InternetKey.SetValue("ProxyServer", PacAddress.Text);
+            InternetKey.SetValue("ProxyEnable", 1);
+
+            ProxyServer = (string) InternetKey.GetValue("ProxyServer");
+            if (ProxyServer == null)
+            {
+                StatusLabel.Text = "Set Proxy Server failure";
+                InternetKey.SetValue("ProxyEnable", 0);
+                return;
+            }
+            StatusLabel.Text = "Set Proxy Server success, address is " + ProxyServer;
+            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
+            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+
+            StatusLabel.Text = "Set Proxy Server success, address is " + ProxyServer;
+            btpac.Enabled = false;
+            btserver.Enabled = false;
+        }
+
+        private void PacAddress_TextChanged(object sender, EventArgs e)
+        {
+            btpac.Enabled = true;
+            btserver.Enabled = true;
+        }
+
+        private void PacAddress_TextEnter(object sender, EventArgs e)
+        {   
+            TextBox tb = (TextBox)sender;
+            ToolTip tt = new ToolTip();
+            tt.Show("输入代理服务器地址或URL, 如:127.0.0.1:8888或http://127.0.0.1/autoproxy.pac", tb, 0, 20, 1500); // milliseconds
+        }
+
+        private void PAC_tip(object sender, EventArgs e)
+        {
+            ToolTip tt = new ToolTip();
+            tt.Show("设置自动代理 PAC URL, 如http://127.0.0.1/autoproxy.pac", this, 10, 140, 1000);
+        }
+
+        private void ProxyServer_Tip(object sender, EventArgs e)
+        {
+            ToolTip tt = new ToolTip();
+            tt.Show("设置代理器地址, 如127.0.0.1:8888", this, 10, 140, 1000);
+        }
+
+        private void startup_Click(object sender, EventArgs e)
+        {
+            if (PacAddress.Text.Length == 0)
+                return;
+                        
+            DialogResult result = MessageBox.Show("URL is " + PacAddress.Text, "自动代理", MessageBoxButtons.YesNo);
+            if (result != System.Windows.Forms.DialogResult.Yes)
+                return;
+
+            InternetKey.SetValue("AutoConfigURL", PacAddress.Text);
+            InternetKey.SetValue("ProxyEnable", 0);
+
+            ProxyPacURL = (string)InternetKey.GetValue("AutoConfigURL");
+            if (ProxyPacURL == null)
+            {
+                StatusLabel.Text = "Set PAC failure";                
+                return;
+            }
+            StatusLabel.Text = "Set PAC success, URL is " + ProxyPacURL;
+
+            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
+            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+
+            btpac.Enabled = false;
+            btserver.Enabled = false;
+        }
+
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            InternetKey.SetValue("ProxyEnable", 0);
+            if (InternetKey.GetValue("AutoConfigURL") != null)
+                InternetKey.DeleteValue("AutoConfigURL");
+            ProxyEnable = (int)InternetKey.GetValue("ProxyEnable");
+            if (ProxyEnable != 0)
+            {
+                StatusLabel.Text = "Clear Proxy Setting failure";
+            }
+            else
+            {
+                StatusLabel.Text = "Proxy Setting has been disabled";
+            }
+
+            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
+            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+
+            btpac.Enabled = true;
+            btserver.Enabled = true;
+        }
+    }
+}
