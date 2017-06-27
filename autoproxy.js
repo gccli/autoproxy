d7b2f24d59b57e66720e7862e6ffd306692350c7
Jing Li <lijing@example.com>
2017-06-27 02:41:19 +0000

    modify proxy port

--- autoproxy.js
+++ autoproxy.js
@@ -1,8 +1,7 @@
 function FindProxyForURL(url, host) {
-    var squid = 'PROXY 192.168.1.102:3128';
     var local = 'PROXY 127.0.0.1:8888';
     var nginx = 'PROXY 10.16.13.18:8080';
-    var autoproxy = 'PROXY 127.0.0.1:8000';
+    var autoproxy = 'PROXY 127.0.0.1:8844';
 
     var patterns = {
         'google': autoproxy,
@@ -15,7 +14,7 @@ function FindProxyForURL(url, host) {
     for (var regex in patterns) {
         if (host.match(regex)) {
             logstr = host + " match regex /" + regex + "/ using " + patterns[regex];
-            alert(logstr);
+            console.log(logstr);
             return patterns[regex];
         }
     }
